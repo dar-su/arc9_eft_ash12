424f4eca6615016d3d79b76a5c49f9dd4491ddc4
Darsu <95997959+dar-su@users.noreply.github.com>
2023-03-22 20:41:28 +0700

    ergo breath and ads volume

--- lua/weapons/arc9_eft_ash12.lua
+++ lua/weapons/arc9_eft_ash12.lua
@@ -342,6 +342,8 @@ SWEP.ToggleAttSound = "" -- we will have own in sound tables
 SWEP.DryFireSound = "" -- we will have own in sound tables
 
 
+SWEP.EnterSightsSound = "arc9_eft_shared/weap_in.wav"
+SWEP.ExitSightsSound = "arc9_eft_shared/weap_handoff.wav"
 
 
 
@@ -1027,8 +1029,9 @@ SWEP.Attachments = {
 SWEP.EFTErgo = 55
 if ARC9EFTBASE then
     SWEP.AimDownSightsTimeHook = ARC9EFT.ErgoHook
-    if ARC9EFT.ErgoBreathHook then -- ash quick fix
+    if ARC9EFT.ErgoBreathHook then
         SWEP.HoldBreathTimeHook = ARC9EFT.ErgoBreathHook
+        SWEP.HookP_TranslateSound = ARC9EFT.ErgoAdsVolume
     end
 else
     print("Dum! install arc9 eft shared!!!!!!!!!!!!!!")
